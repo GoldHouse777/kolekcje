c66c3d16d1abf0e8495a6a68760cfcf657cb6401
GoldHouse777 <psoltysik01@gmail.com>
2024-09-12 10:37:19 +0200

    losowanie liczb w tablicy

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //TABLICA
+        /*
+        -tablica musi miec zdefiniowant rozmiar, ktorego potem nie mozemy zmienic.
+        -tablica przechowuje typy proste i zlozone.
+         */
+
+        int [] tablicaLiczbLosowych = new int[6];
+        //losujemy wartosci z zakresu od 1 do 100
+        for (int i = 0; i < tablicaLiczbLosowych.length; i++) {
+            tablicaLiczbLosowych[i] = (int)(Math.random()*100);
+        }
+        //wypisywanie tablicy na ekranie
+        for (int element:tablicaLiczbLosowych) {
+            System.out.println(element+", ");
+        }
     }
 }
