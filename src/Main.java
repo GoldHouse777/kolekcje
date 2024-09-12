44b6fe1e90131f61d706107da69134203c431112
GoldHouse777 <psoltysik01@gmail.com>
2024-09-12 11:02:19 +0200

    wypelnianie kolekcji wartosciami losowymi

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         //TABLICA
         /*
-        -tablica musi miec zdefiniowant rozmiar, ktorego potem nie mozemy zmienic.
-        -tablica przechowuje typy proste i zlozone.
+        - tablica musi miec zdefiniowant rozmiar, ktorego potem nie mozemy zmienic.
+        - tablica przechowuje typy proste i zlozone.
          */
 
         int [] tablicaLiczbLosowych = new int[6];
@@ -16,5 +19,23 @@ public class Main {
         for (int element:tablicaLiczbLosowych) {
             System.out.println(element+", ");
         }
+        //wypelnianie kolekcji wartosciami losowymi
+        /*
+        - kolekcja moze przechowywac tylko typy zlozone np. Integer
+        - kolekcja nie musi miec zdefiniowanego rozmiaru, rozmiar moze sie zmieniac w trakcie
+        - List -> ArrayList, LinkedList,
+        - Set -> HashSet
+         */
+
+        List<Integer> listaLiczbLosowych = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            int liczba = (int)(Math.random()*100);
+            listaLiczbLosowych.add(liczba);
+        }
+        System.out.println("Wylosowana liczba");
+        for (Integer element:listaLiczbLosowych) {
+            System.out.println(element+", ");
+        }
+        System.out.println(listaLiczbLosowych);
     }
 }
